ca76798d416af1022615980362c7acd134826251
MisterBlizzard <ax-filipe@hotmail.com>
2018-05-11 11:39:14 +0000

    Initizalize Project

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # os namespaces declarados devem ter uma pasta própria dentro da pasta controllers, é uma forma de organização
+  # no path pode ser passado apenas o "/", chamando algo como www.site.com/api/tasks
+  # se passar algo como path: "backend", seria www.site.com/backend/tasks
+  # o domínio pode ser acessado com o subdominio declarado na constraint (api.site.com/tasks)
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
+    
+  end
+end
